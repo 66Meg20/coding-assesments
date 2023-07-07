ade840df3a5195c960f10ab094202c97584c59cf
66Meg20 <66meg.dj2066@gmail.com>
2023-07-06 21:17:05 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// let numArray = [5, 6, 8, 9, 1, 2, 3];
+// numArray.sort((a, b) => a - b);
+// console.log(numArray);
+
+function sortedSquaredArray(array) {
+  array.sort((a, b) => a - b);
+}
+
+let array = [1, 2, 3, 5, 6, 8, 9];
+
+console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9]));
